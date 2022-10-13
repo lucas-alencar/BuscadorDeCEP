d80e7f708021f0971d24205ef5ab4c2d91d5f795
lucas-alencar <lucasalencar.rd@gmail.com>
2022-10-13 20:02:45 -0300

    doing initial design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div className="container">
+      <h1 className='title'>Buscador de Cep</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep..."
+        />
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 40028922</h2>
+        <span>Rua do Yudi Tamashiro</span>
+        <span>Complemento: Vila Cesamo</span>
+      </main>
+
     </div>
   );
 }
