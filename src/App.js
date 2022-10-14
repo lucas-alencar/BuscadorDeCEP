805c8aa711bc6bcfd558ce87e62b2c2d5dfeff67
lucas-alencar <lucasalencar.rd@gmail.com>
2022-10-13 22:30:53 -0300

    adding api - zip code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
+import api from './services/api';
 import './styles.css';
 
+
 function App() {
+
+  const [input, setInput] = useState('')
+
+// api: 59124120/json
+
+  async function handleSearch(){
+    if(input !== ''){
+      try{
+        const response = await api.get(`${input}/json`);
+        console.log(response);
+      }catch{
+
+      }
+    }
+  }
+
   return (
     <div className="container">
       <h1 className='title'>Buscador de Cep</h1>
 
       <div className="containerInput">
-        <input
-        type="text"
-        placeholder="Digite seu cep..."
-        />
-        <button className="buttonSearch">
-          <FiSearch size={25} color="#fff" />
-        </button>
+          <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value) }
+          />
+          <button className="buttonSearch" onClick={handleSearch()}>
+            <FiSearch size={25} color="#fff" />
+          </button>
       </div>
 
       <main className='main'>
